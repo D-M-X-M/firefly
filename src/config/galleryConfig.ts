decdfb3833b48a9abc5a50a07e420604f0c0266b
D-M-X-M <2016877928@qq.com>
2026-08-16 09:47:09 +0800

    更新网站 2026-08-16 09:47

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -34,6 +34,14 @@ export const galleryConfig: GalleryConfig = {
 		// 	password: "123456",
 		// 	passwordHint: "示例密码123456",
 		// },
+		{
+			id: "lover",
+			name: "我和另一半...",
+			description: "幸福，我要的幸福，渐渐清楚...",
+			location: "福州",
+			date: "2026-08-16",
+			tags: ["爱情"],
+		},
 	],
 
 	// 瀑布流最小列宽(px)，浏览器根据容器宽度自动计算列数，默认 240
