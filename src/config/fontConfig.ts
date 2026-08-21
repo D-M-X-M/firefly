a29c3ab287197ba7e768961ac043f8768d3ea540
D-M-X-M <2016877928@qq.com>
2026-08-21 20:18:19 +0800

    更新网站 2026-08-21 20:18

--- src/config/fontConfig.ts
+++ src/config/fontConfig.ts
@@ -81,15 +81,15 @@ export const fontsList: FontDefinition[] = [
 		},
 		fallbacks: ["sans-serif"],
 	},
-	{
-		name: "Noto Serif SC",
-		cssVariable: "--font-noto-serif-sc",
-		provider: "fontsource",          // 使用 Google Fonts 内置支持
-		weights: ["400", "700"],     // 常规和粗体（可按需增加）
-		styles: ["normal"],
-		subsets: ["latin", "chinese-simplified"],
-		fallbacks: ["serif"],
-	},
+	// {
+	// 	name: "Noto Serif SC",
+	// 	cssVariable: "--font-noto-serif-sc",
+	// 	provider: "fontsource",          // 使用 Google Fonts 内置支持
+	// 	weights: ["400", "700"],     // 常规和粗体（可按需增加）
+	// 	styles: ["normal"],
+	// 	subsets: ["latin", "chinese-simplified"],
+	// 	fallbacks: ["serif"],
+	// },
 	
 ];
 
@@ -99,8 +99,8 @@ export const fontConfig: FontSelectionConfig = {
 	enable: true,
 	// 当前选择的字体 CSS 变量名（对应上方 fonts 中的 cssVariable）
 	// 使用 "system" 表示系统字体（不加载任何自定义字体）
-	selected: ["--font-noto-serif-sc"],
-	// selected: ["system"],
+	// selected: ["--font-noto-serif-sc"],
+	selected: ["system"],
 
 	// 各区域独立字体设置（填写上方 fonts 中的 cssVariable，留空则使用全局 selected 字体）
 	// 例如：bannerTitleFont: "--font-inter", 表示主页横幅主标题使用 Inter 字体
